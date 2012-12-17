30bef70abfa9e20c96b4011e4ea685579e365b73
Dont.L <Dont.L@gmail.com>
2012-12-17 20:54:14 +0800

    ssp_ls added

---
+++ intro/ssp_ls.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    DIR *dir;
+    struct dirent *dirent;
+
+    if(argc!=2)
+    {
+        perror("too less paramter .");
+        exit(0);
+    }   
+        if((dir=opendir(argv[1]))==NULL)
+        {
+            perror("opendir failed");
+            exit(0);
+        }
+    
+    while((dirent=readdir(dir))!=NULL)
+        printf("%s\n",dirent->d_name);
+
+    closedir(dir);
+
+    return (0);
+}
